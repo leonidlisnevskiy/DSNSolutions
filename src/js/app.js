42a2ed09e96cae7222d11c76651ad3be7712c05a
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2018-02-20 17:52:30 +0200

    add svg4everybody

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
-
+//POLYFILL SVGXUSE
+import svg4everybody from 'svg4everybody';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'owl.carousel/dist/owl.carousel';
 import './components';
+
+svg4everybody();
